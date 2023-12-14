9af900292e916ae977e28c2e8e9c137afd935718
Tahir Ahmed Khan <148902580+TahirCodeCraft@users.noreply.github.com>
2023-12-14 23:10:04 +0500

    Add files via upload

---
+++ class02/task01.js
@@ -0,0 +1,18 @@
+console.log("Hello class 1");
+let a = "Tahir"; //type inferance
+// a = 23 Error
+let b = "TS 011"; //explicity define type
+// firstName - camelcase - single line comment
+let first_name1;
+/*
+multiple line comment
+console.log(a)
+console.log(b)
+*/
+const c = null; //explicit typing
+console.log(c);
+let d = undefined;
+console.log(d);
+let e = true;
+console.log(typeof (e));
+export {};
