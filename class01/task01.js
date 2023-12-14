374afc10636ecc31bd25b371b2cc3e48499b186d
Tahir Ahmed Khan <148902580+TahirCodeCraft@users.noreply.github.com>
2023-12-14 22:47:24 +0500

    Add files via upload

---
+++ class01/task01.js
@@ -0,0 +1,5 @@
+//print hello world in console.log
+console.log("Hello World");
+//print your name
+console.log("Tahir, How are you");
+export {};
---
+++ class01/task01.ts
@@ -0,0 +1,7 @@
+//print hello world in console.log
+
+console.log("Hello World");
+
+//print your name
+
+console.log("Tahir, How are you");
